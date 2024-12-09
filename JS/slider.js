4ca9233d822199f576b665573b43e2f198cf76e6
Melvin1032 <144808488+Melvin1032@users.noreply.github.com>
2024-12-10 00:51:11 +0800

    Added the Academic Page

--- JS/slider.js
+++ JS/slider.js
@@ -23,3 +23,30 @@ function showSlides() {
 }
 
 showSlides();
+
+let slideIndexPrograms = 0;
+
+function showProgramSlides() {
+    let slides = document.getElementsByClassName("mySlides2");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = "0";
+        slides[i].style.transition = "opacity 1s ease-in-out";
+        slides[i].style.display = "none";
+    }
+
+    slideIndexPrograms++;
+    if (slideIndexPrograms > slides.length) {
+        slideIndexPrograms = 1;
+    }
+
+    slides[slideIndexPrograms - 1].style.display = "block";
+    setTimeout(() => {
+        slides[slideIndexPrograms - 1].style.opacity = "1";
+    }, 50);
+
+    setTimeout(showProgramSlides, 6000); 
+}
+
+showProgramSlides();
+
